c8395adbb0d9cb88681746d2addf059272859b4c
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-03-18 16:13:15 +0100

    Configuration de la methode addMedicalRecords

--- SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/controller/MedicalRecordsController.java
+++ SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/controller/MedicalRecordsController.java
@@ -3,26 +3,53 @@ package com.openclassrooms.safetynetalerts.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.safetynetalerts.model.MedicalRecords;
 import com.openclassrooms.safetynetalerts.service.MedicalRecordsService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class MedicalRecordsController {
 
 	@Autowired
 	private MedicalRecordsService medicalRecordsService;
-	
+
 	@GetMapping("/medicalrecord")
 	public List<MedicalRecords> getAllMedicalRecords() throws Exception {
 		return medicalRecordsService.getAllMedicalRecords();
 	}
-	
+
 	@PostMapping("medicalrecord")
-	public MedicalRecords saveNewMedicalrecord() throws Exception {
-		return medicalRecordsService.addMedicalRecord();
+	public ResponseEntity<String> addNewMedicalrecord(@Valid @RequestBody MedicalRecords newMedicalRecords) {
+
+		try {
+			medicalRecordsService.addMedicalRecord(newMedicalRecords);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Rapport medical enregistré avec succès !");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Erreur lors de l'ajout du rapport médical: " + e.getMessage());
+		}
+
+	}
+
+	@PutMapping("medicalrecord/{firstname}/{lastname}")
+	public ResponseEntity<?> updateMedicalRecord(@PathVariable String firstName, @PathVariable String lastName, @RequestBody MedicalRecords updatedMedicalRecord) throws Exception {
+		MedicalRecords updateMedicalRecord = medicalRecordsService.updateMedicalRecord(firstName, lastName, updatedMedicalRecord);
+		
+		if (updateMedicalRecord != null) {
+			return ResponseEntity.ok(updateMedicalRecord);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dossier medical non trouvé");
+		}
+
 	}
 }
--- SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/MedicalRecordsService.java
+++ SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/MedicalRecordsService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
@@ -20,50 +19,79 @@ import com.openclassrooms.safetynetalerts.model.MedicalRecords;
 
 @Service
 public class MedicalRecordsService {
-	
+
 	@Autowired
 	private final CustomProperties jsonFile;
-	
+
 	Gson gson = new Gson();
-	
+
+	private String jsonFilePath;
+
 	public MedicalRecordsService(CustomProperties jsonFile) {
 		this.jsonFile = jsonFile;
 	}
-	
+
 	public List<MedicalRecords> getAllMedicalRecords() throws Exception {
-		String jsonFilePath = jsonFile.getJsonFile();
+		jsonFilePath = jsonFile.getJsonFile();
 		InputStream in = getClass().getResourceAsStream("/" + jsonFilePath);
-		
+
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			JsonObject medicalRecordsJson = gson.fromJson(br,JsonObject.class);
+			JsonObject medicalRecordsJson = gson.fromJson(br, JsonObject.class);
 			String response = medicalRecordsJson.get("medicalrecords").toString();
-			
-			Type medicalRecordsListType = new TypeToken<List<MedicalRecords>>(){}.getType();
+
+			Type medicalRecordsListType = new TypeToken<List<MedicalRecords>>() {
+			}.getType();
 			return gson.fromJson(response, medicalRecordsListType);
-			
-		
+
 		} catch (JsonSyntaxException e) {
-	        throw new RuntimeException("Erreur lors de la lecture du fichier JSON", e);
-	    }
+			throw new RuntimeException("Erreur lors de la lecture du fichier JSON", e);
+		}
 	}
 
-	public MedicalRecords addMedicalRecord() throws Exception {
-		String jsonFilePath = jsonFile.getJsonFile();
-		InputStream in = getClass().getResourceAsStream("/" + jsonFilePath);
-		
-		try {
-			FileWriter writer = new FileWriter("data.json");
-			JsonElement record = null;
-			gson.toJson(record, writer);
-			writer.close();
+	public void addMedicalRecord(MedicalRecords newMedicalRecords) throws Exception {
+		List<MedicalRecords> allMedicalRecordsList = getAllMedicalRecords();
 
-		} catch (JsonSyntaxException e){
+		allMedicalRecordsList.add(newMedicalRecords);
+
+		jsonFilePath = jsonFile.getJsonFile();
+		FileWriter fileWriter = new FileWriter(getClass().getResource("/" + jsonFilePath).getFile());
+		JsonObject medicalrecordsJson = new JsonObject();
+		medicalrecordsJson.add("medicalrecords", gson.toJsonTree(allMedicalRecordsList));
+
+		try {
+			gson.toJson(medicalrecordsJson, fileWriter);
+			fileWriter.close();
+		} catch (JsonSyntaxException e) {
 			throw new RuntimeException("Erreur lors de l'envoie vers le fichier JSON", e);
 		}
+
+	}
+	
+	private void saveMedicalRecordsToJson(List<MedicalRecords> allMedicalRecordsList) throws Exception {
+	    String jsonFilePath = jsonFile.getJsonFile();
+	    FileWriter writer = new FileWriter(getClass().getResource("/" + jsonFilePath).getFile()); 
+	    
+	    gson.toJson(allMedicalRecordsList, writer);
+	    writer.flush();
+	    writer.close();
+	}
+
+	public MedicalRecords updateMedicalRecord(String firstName, String lastName, MedicalRecords updateMedicalRecords) throws Exception {
+		List<MedicalRecords> allMedicalRecordsList = getAllMedicalRecords();
 		
+		for (MedicalRecords record : allMedicalRecordsList) {
+			if (record.getFirstName().equalsIgnoreCase(firstName) && record.getLastName().equalsIgnoreCase(lastName)) {
+				record.setBirthDate(updateMedicalRecords.getBirthDate());
+				record.setMedications(updateMedicalRecords.getMedications());
+				record.setAllergies(updateMedicalRecords.getAllergies());
+				
+				saveMedicalRecordsToJson(allMedicalRecordsList);
+				
+				return record;
+			}
+		}
 		return null;
-		
+
 	}
 }
-
