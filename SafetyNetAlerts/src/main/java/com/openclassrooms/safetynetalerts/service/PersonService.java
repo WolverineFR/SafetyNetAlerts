66b9dd764143028f6f632938551e3dbe8f44cd84
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-04-10 14:16:40 +0200

    Mise à jour du code dans PersonService -Mise à jour dans l'url getChildrenByAddress

--- SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/PersonService.java
+++ SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/PersonService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.safetynetalerts.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,8 @@ public class PersonService {
 	private final PersonRepository personRepository;
 	private final FireStationRepository fireStationRepository;
 
-	public PersonService(MedicalRecordsService medicalRecordsService, PersonRepository personRepository,FireStationRepository fireStationRepository) {
+	public PersonService(MedicalRecordsService medicalRecordsService, PersonRepository personRepository,
+			FireStationRepository fireStationRepository) {
 		this.medicalRecordsService = medicalRecordsService;
 		this.personRepository = personRepository;
 		this.fireStationRepository = fireStationRepository;
@@ -39,63 +41,66 @@ public class PersonService {
 
 	// Ajouter une Personne
 	public Person addPerson(Person newPerson) throws PersonException {
-		if (newPerson.getFirstName() == null || newPerson.getFirstName().isBlank() || newPerson.getLastName() == null || newPerson.getLastName().isBlank()) {
+		if (newPerson.getFirstName() == null || newPerson.getFirstName().isBlank() || newPerson.getLastName() == null
+				|| newPerson.getLastName().isBlank()) {
 			throw new PersonException("Le prénom et/ou le nom de la personne ne peuvent pas être vides.");
 		}
-		
-		if (newPerson.getAddress() == null || newPerson.getAddress().isBlank() || newPerson.getCity() == null || newPerson.getCity().isBlank() || newPerson.getZip() == null || newPerson.getZip().isBlank()) {
+
+		if (newPerson.getAddress() == null || newPerson.getAddress().isBlank() || newPerson.getCity() == null
+				|| newPerson.getCity().isBlank() || newPerson.getZip() == null || newPerson.getZip().isBlank()) {
 			throw new PersonException("L'adresse de la personne ne peut pas être vide.");
 		}
-		
+
 		if (newPerson.getPhone() == null || newPerson.getPhone().isBlank()) {
 			throw new PersonException("Le numéro de téléphone de la personne ne peut pas être vide.");
 		}
-		
+
 		if (newPerson.getEmail() == null || newPerson.getEmail().isBlank()) {
 			throw new PersonException("L'email de la personne ne peut pas être vide.");
 		}
-		
-		 List<MedicalRecords> medicalRecords = medicalRecordsService.getAllMedicalRecords();
-		    boolean hasMedicalRecord = medicalRecords.stream().anyMatch(mr ->
-		        mr.getFirstName().equalsIgnoreCase(newPerson.getFirstName()) &&
-		        mr.getLastName().equalsIgnoreCase(newPerson.getLastName())
-		    );
 
-		    if (!hasMedicalRecord) {
-		        throw new RuntimeException("Aucun dossier médical trouvé pour cette personne.");
-		    }
+		List<MedicalRecords> medicalRecords = medicalRecordsService.getAllMedicalRecords();
+		boolean hasMedicalRecord = medicalRecords.stream()
+				.anyMatch(mr -> mr.getFirstName().equalsIgnoreCase(newPerson.getFirstName())
+						&& mr.getLastName().equalsIgnoreCase(newPerson.getLastName()));
 
-		    boolean addressCovered = fireStationRepository.getAllFireStation().stream().anyMatch(fs ->
-		        fs.getAddress().equalsIgnoreCase(newPerson.getAddress())
-		    );
+		if (!hasMedicalRecord) {
+			throw new RuntimeException("Aucun dossier médical trouvé pour cette personne.");
+		}
 
-		    if (!addressCovered) {
-		        throw new RuntimeException("L'adresse de la personne n'est pas couverte par une caserne.");
-		    }
-		
-	return personRepository.addPerson(newPerson);
+		boolean addressCovered = fireStationRepository.getAllFireStation().stream()
+				.anyMatch(fs -> fs.getAddress().equalsIgnoreCase(newPerson.getAddress()));
+
+		if (!addressCovered) {
+			throw new RuntimeException("L'adresse de la personne n'est pas couverte par une caserne.");
+		}
+
+		return personRepository.addPerson(newPerson);
 	}
 
 	// Mise à jour des données
-	public Person updatePerson(String firstName, String lastName, Person updatePerson) throws ResourceNotFoundException {
-		 if (!firstName.equalsIgnoreCase(updatePerson.getFirstName()) ||
-			        !lastName.equalsIgnoreCase(updatePerson.getLastName())) {
-			        throw new IllegalArgumentException("Le prénom et/ou le nom de l'URL ne correspondent pas à ceux du corps de la requête.");
-			    }
-		
-		return personRepository.updatePerson(firstName,lastName,updatePerson);
+	public Person updatePerson(String firstName, String lastName, Person updatePerson)
+			throws ResourceNotFoundException {
+		if (!firstName.equalsIgnoreCase(updatePerson.getFirstName())
+				|| !lastName.equalsIgnoreCase(updatePerson.getLastName())) {
+			throw new IllegalArgumentException(
+					"Le prénom et/ou le nom de l'URL ne correspondent pas à ceux du corps de la requête.");
+		}
+
+		return personRepository.updatePerson(firstName, lastName, updatePerson);
 	}
 
 	// Supression d'une personne
 	public void deletePerson(String firstName, String lastName) throws ResourceNotFoundException {
 		List<Person> allPersons = getAllPerson();
-		boolean removed = allPersons.removeIf(person -> person.getFirstName().equalsIgnoreCase(firstName) && person.getLastName().equalsIgnoreCase(lastName));
-		
-		    if (removed) {
-		        personRepository.savePersonToJson(allPersons);
-		    } else {
-		    	 throw new ResourceNotFoundException ("Aucune personne ne correspond à cette requete");
-		    }
+		boolean removed = allPersons.removeIf(person -> person.getFirstName().equalsIgnoreCase(firstName)
+				&& person.getLastName().equalsIgnoreCase(lastName));
+
+		if (removed) {
+			personRepository.savePersonToJson(allPersons);
+		} else {
+			throw new ResourceNotFoundException("Aucune personne ne correspond à cette requete");
+		}
 	}
 
 	/// URL
@@ -106,18 +111,22 @@ public class PersonService {
 		List<MedicalRecords> getAllMedicalRecords = medicalRecordsService.getAllMedicalRecords();
 
 		List<ChildByAddressDTO> filteredPersons = new ArrayList<>();
-		List<String> familyMembers = new ArrayList<>();
 
 		for (Person person : getAllPerson) {
 			if (address.contains(person.getAddress())) {
 				for (MedicalRecords medicalRecord : getAllMedicalRecords) {
 					if (person.getFirstName().equals(medicalRecord.getFirstName())
 							&& person.getLastName().equals(medicalRecord.getLastName())) {
-						if (person.getLastName().equals(medicalRecord.getLastName())) {
-							familyMembers.add(medicalRecord.getFirstName() + " " + medicalRecord.getLastName());
-						}
+
 						int age = medicalRecordsService.calculateAge(medicalRecord);
 						if (age <= 18) {
+
+							List<String> familyMembers = getAllPerson.stream()
+									.filter(pers -> address.equalsIgnoreCase(pers.getAddress())
+											&& !(pers.getFirstName().equalsIgnoreCase(person.getFirstName())
+													&& pers.getLastName().equalsIgnoreCase(person.getLastName())))
+									.map(pers -> pers.getFirstName() + " " + pers.getLastName())
+									.collect(Collectors.toList());
 
 							filteredPersons.add(new ChildByAddressDTO(person.getFirstName(), person.getLastName(), age,
 									familyMembers));
