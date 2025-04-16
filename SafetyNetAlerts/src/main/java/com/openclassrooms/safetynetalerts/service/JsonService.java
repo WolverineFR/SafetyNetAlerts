02195206423921e3e61679f9c64b38a1f51fbf57
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-04-16 16:00:59 +0200

    Commentaire ajoutés pour la classe JsonService

--- SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/JsonService.java
+++ SafetyNetAlerts/src/main/java/com/openclassrooms/safetynetalerts/service/JsonService.java
@@ -10,11 +10,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * Service permettant la lecture et l'écriture de données JSON depuis et vers un fichier.
+ * Ce service centralise l'accès au fichier `data.json` utilisé comme base de données.
+ */
 @Service
 public class JsonService {
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private static final String FILE_PATH = "target/classes/data.json";
 
+	/**
+	 * Lit une catégorie spécifique du fichier JSON et la convertit en un objet Java.
+	 *
+	 * @param <T>         le type de l'objet attendu.
+	 * @param typeOfT     la référence de type pour la désérialisation.
+	 * @param category    le nom de la catégorie à lire (ex: "persons", "firestations"...).
+	 * @return            un objet de type T correspondant à la catégorie lue.
+	 * @throws RuntimeException si le fichier ne peut pas être lu ou si la catégorie est absente.
+	 */
 	public <T> T readJsonFromFile(TypeReference<T> typeOfT, String category) {
 		try {
 			JsonNode rootNode = objectMapper.readTree(new File(FILE_PATH));
@@ -28,6 +41,14 @@ public class JsonService {
 		}
 	}
 
+	/**
+	 * Écrit les données dans une catégorie spécifique du fichier JSON.
+	 * Si la catégorie existe, elle est remplacée ; sinon, elle est ajoutée.
+	 *
+	 * @param category : Le nom de la catégorie à modifier ou ajouter.
+	 * @param data     : Les données à écrire (doivent être compatibles JSON).
+	 * @throws RuntimeException si une erreur se produit lors de l'écriture.
+	 */
 	public void writeJsonToFile(String category, Object data) {
 		try {
 			JsonNode rootNode = objectMapper.readTree(new File(FILE_PATH));
